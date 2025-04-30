17d2824629313726291229ca60c22280ed6135d8
Mohammad Atif Shameem Khan <107842948+Kuroo7@users.noreply.github.com>
2025-04-30 13:32:06 +0530

    Update cors

--- server/server.js
+++ server/server.js
@@ -16,10 +16,22 @@ connectDB();
 const app = express();
 
 // Middlewares
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  'http://localhost:5173'
+];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 
